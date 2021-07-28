c22efaddad634ce7272abce8b3aa1e7805bf9ad9
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2021-07-28 11:32:15 +0100

    PP-8370: Update to enable setting proxy_protocol on or off based on environment variables

--- local-testing-config/entrypoint-frontend.sh
+++ local-testing-config/entrypoint-frontend.sh
@@ -4,4 +4,13 @@ set -e
 
 apk add nginx-mod-stream
 
+PROXY_PROTOCOL=${PROXY_PROTOCOL:-off}
+if [ "$PROXY_PROTOCOL" != "on" ] && [ "$PROXY_PROTOCOL" != "off" ]; then
+  echo "Error: PROXY_PROTOCOL env var must either be 'on' or 'off'. It was $PROXY_PROTOCOL"
+  exit 1
+fi
+
+NGINX_CONFIG="/etc/nginx/nginx.conf"
+sed "s,PROXY_PROTOCOL,${PROXY_PROTOCOL},g" $NGINX_CONFIG.default > $NGINX_CONFIG
+
 exec nginx -g 'daemon off;'
--- src/docker-startup.sh
+++ src/docker-startup.sh
@@ -5,7 +5,20 @@ set -e
 TCP_CONFIG="/etc/nginx/tcpconf.d/tcp.stream"
 
 SERVER_DNS=${SERVER_DNS:-"localhost"}
-
 sed -i "s,SERVER_DNS,${SERVER_DNS},g" $TCP_CONFIG
+
+PROXY_PROTOCOL=${PROXY_PROTOCOL:-off}
+if [ "$PROXY_PROTOCOL" = "on" ]; then
+  sed -i "s,PROXY_PROTOCOL,proxy_protocol,g" $TCP_CONFIG
+  # shellcheck disable=SC2016
+  sed -i 's,REMOTE_ADDR_LOG_FORMAT,$proxy_protocol_addr:$proxy_protocol_port $proxy_protocol_server_addr,g' $TCP_CONFIG
+elif [ "$PROXY_PROTOCOL" = "off" ]; then
+  sed -i "s,PROXY_PROTOCOL,,g" $TCP_CONFIG
+  # shellcheck disable=SC2016
+  sed -i 's,REMOTE_ADDR_LOG_FORMAT,$remote_addr,g' $TCP_CONFIG
+else
+  echo "Error: PROXY_PROTOCOL env var must either be 'on' or 'off'. It was $PROXY_PROTOCOL"
+  exit 1
+fi
 
 nginx -g "daemon off;"
